69beaa3d4efda520b3ded10f7908801c538a0402
Will Farrington <wcfarrington@gmail.com>
2009-04-10 19:29:29 -0400

    Add solutions for 3.2

--- test-3.rb
+++ test-3.rb
@@ -10,9 +10,41 @@ leslie = Matrix[[0,  1.2, 1.1,0.9,0.1 ,0  ,0   ,0   ,0],
                 [0,  0,   0,  0,  0,   0,  0.77,0   ,0],
                 [0,  0,   0,  0,  0,   0,  0,   0.40,0]]
 
-x_0 = Matrix[[2.1], [1.9], [1.8], [2.1], [2.0], [1.7], [1.2], [0.9], [0.5]]
+# population distributions
 
-#puts leslie.power_method
+x_0 = Matrix[[2.1], [1.9], [1.8], [2.1], [2.0], [1.7], [1.2], [0.9], [0.5]]
+x_1 = leslie * x_0
+x_2 = leslie * x_1
+x_3 = leslie * x_2
+x_4 = leslie * x_3
+x_5 = leslie * x_4
+
+# total populations
+
+populations = [
+               x_0.to_a.flatten.inject(&:+) * 10000,
+               x_1.to_a.flatten.inject(&:+) * 10000,
+               x_2.to_a.flatten.inject(&:+) * 10000,
+               x_3.to_a.flatten.inject(&:+) * 10000,
+               x_4.to_a.flatten.inject(&:+) * 10000,
+               x_5.to_a.flatten.inject(&:+) * 10000
+              ]
+
+puts "2010: #{populations[1]}"
+puts "2020: #{populations[2]}"
+puts "2030: #{populations[3]}"
+puts "2040: #{populations[4]}"
+puts "2050: #{populations[5]}"
+
+# percentage of population change
+
+puts "2000 -> 2010: %.2f\%" % ((populations[1] - populations[0]) / populations[0] * 100)
+puts "2010 -> 2020: %.2f\%" % ((populations[2] - populations[1]) / populations[1] * 100)
+puts "2020 -> 2030: %.2f\%" % ((populations[3] - populations[2]) / populations[2] * 100)
+puts "2030 -> 2040: %.2f\%" % ((populations[4] - populations[3]) / populations[3] * 100)
+puts "2040 -> 2050: %.2f\%" % ((populations[5] - populations[4]) / populations[4] * 100)
+
+# puts leslie.power_method
 
 # assuming x(n) = Ax(n-1), and A never changes,
 #
