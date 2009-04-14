59164b201d6e47dcad52acf50053af3add789dd7
Will Farrington <wcfarrington@gmail.com>
2009-04-14 04:20:20 -0400

    Formatting cleanup

--- code/ConvolutionalCode.java
+++ code/ConvolutionalCode.java
@@ -2,97 +2,73 @@ import java.util.Random;
 
 public class ConvolutionalCode
 {
-	public static void main (String[] args)
-	{
-	// 	Random generator = new Random();
-// 		int i;
-// 		// Matrix input = Matrix.generateInput();
-// 		Matrix input = new Matrix(10,1);
-// 		for (i = 0; i < input.length; i++)
-// 		{
-// 			for (int j = 0; j < input.entry[0].length; j += 2)
-// 				input.entry[i][j] = generator.nextInt(2);
-// 		}	
-// 		System.out.print(input);
-// 				
-// 		
-//  		Matrix A = new Matrix(input.length, input.length);
-//  		Matrix Y, X;
-//  		A = A.fillMatrixZero();
-// 		Y = A.matrixMultiply(input);
-// 		X = Y.gaussseidelIteration(A, 5);
-//  		System.out.print(A);
-// 		System.out.print(input);
-// 		System.out.print(Y);
-// 		System.out.print(X);
+    public static void main (String[] args)
+    {
 
-	Matrix input, output, Y0, Y1, A, B;
-	
-	//Encode specific input sequence
-	double[] vector = {1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0};
-	input = new Matrix(vector);
-	System.out.print(input);
-	System.out.println("Encoding...");
-	
-	//Encodes the Y0 output stream for the input
-	A = new Matrix(input.length, input.length);
-	A = A.fillMatrixZero();
-	Y0 = A.matrixMultiply(input);
-	System.out.print(Y0);
-	
-	//Encode the Y1 output stream for the input
-	B = new Matrix(input.length, input.length);
-	B = B.fillMatrixOne();
-	Y1 = B.matrixMultiply(input);
-	System.out.print(Y1);
-	
-	//Generates a random input stream and encodes both output streams
-	input = Matrix.generateInput();
-	A = new Matrix(input.length, input.length);
-	A = A.fillMatrixZero();
-	B = new Matrix(input.length, input.length);
-	B = B.fillMatrixOne();
-	Y0 = A.matrixMultiply(input);
-	Y1 = B.matrixMultiply(input);
-	System.out.println("Generating input...");
-	System.out.print(input);
-	System.out.print("Encoding...");
-	System.out.print(Y0);
-	System.out.print(Y1);
-	
-	//Generates two random output stream and decodes their input streams using
-	//Jacobi and Gauss-Seidel iterations. The first random stream is decoded as
-	//a Y0 stream and the second as a Y1 stream. Both iterative methods are fun 8 times.
-	int maxit = 8;
-	System.out.println("Generating random output...");
-	output = Matrix.generateOutput();
-	A = new Matrix(output.length, output.length);
-	A = A.fillMatrixZero();
-	B = new Matrix(output.length, output.length);
-	B = B.fillMatrixOne();
-	System.out.print(output);
-	System.out.println("Decoding...");
-	input = output.jacobiIteration(A, maxit);
-	System.out.print(input);
-	input = output.gaussseidelIteration(A, maxit);
-	System.out.print(input);
-	
-	System.out.println("Generating random output...");
-	output = Matrix.generateOutput();
-	A = new Matrix(output.length, output.length);
-	A = A.fillMatrixZero();
-	B = new Matrix(output.length, output.length);
-	B = B.fillMatrixOne();
-	System.out.print(output);
-	System.out.println("Decoding...");
-	input = output.jacobiIteration(B, maxit);
-	System.out.print(input);
-	input = output.gaussseidelIteration(B, maxit);
-	System.out.print(input);
-	
-	System.out.print("Done");
-	
-	
-	
-	}
+        Matrix input, output, Y0, Y1, A, B;
+    
+        //Encode specific input sequence
+        double[] vector = {1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0};
+        input = new Matrix(vector);
+        System.out.print(input);
+        System.out.println("Encoding...");
+    
+        //Encodes the Y0 output stream for the input
+        A = new Matrix(input.length, input.length);
+        A = A.fillMatrixZero();
+        Y0 = A.matrixMultiply(input);
+        System.out.print(Y0);
+
+        //Encode the Y1 output stream for the input
+        B = new Matrix(input.length, input.length);
+        B = B.fillMatrixOne();
+        Y1 = B.matrixMultiply(input);
+        System.out.print(Y1);
+
+        //Generates a random input stream and encodes both output streams
+        input = Matrix.generateInput();
+        A = new Matrix(input.length, input.length);
+        A = A.fillMatrixZero();
+        B = new Matrix(input.length, input.length);
+        B = B.fillMatrixOne();
+        Y0 = A.matrixMultiply(input);
+        Y1 = B.matrixMultiply(input);
+        System.out.println("Generating input...");
+        System.out.print(input);
+        System.out.print("Encoding...");
+        System.out.print(Y0);
+        System.out.print(Y1);
+
+        //Generates two random output stream and decodes their input streams using
+        //Jacobi and Gauss-Seidel iterations. The first random stream is decoded as
+        //a Y0 stream and the second as a Y1 stream. Both iterative methods are fun 8 times.
+        int maxit = 8;
+        System.out.println("Generating random output...");
+        output = Matrix.generateOutput();
+        A = new Matrix(output.length, output.length);
+        A = A.fillMatrixZero();
+        B = new Matrix(output.length, output.length);
+        B = B.fillMatrixOne();
+        System.out.print(output);
+        System.out.println("Decoding...");
+        input = output.jacobiIteration(A, maxit);
+        System.out.print(input);
+        input = output.gaussseidelIteration(A, maxit);
+        System.out.print(input);
+
+        System.out.println("Generating random output...");
+        output = Matrix.generateOutput();
+        A = new Matrix(output.length, output.length);
+        A = A.fillMatrixZero();
+        B = new Matrix(output.length, output.length);
+        B = B.fillMatrixOne();
+        System.out.print(output);
+        System.out.println("Decoding...");
+        input = output.jacobiIteration(B, maxit);
+        System.out.print(input);
+        input = output.gaussseidelIteration(B, maxit);
+        System.out.print(input);
+
+        System.out.print("Done");    
+    }
 }
--- code/Matrix.java
+++ code/Matrix.java
@@ -2,168 +2,160 @@ import java.util.Random;
 
 public class Matrix
 {
-	//instance data for matrices
-	double[][] entry;
-	int length;
-	
-	//constructor for matrix object
-	public Matrix(int m, int n)
-	{
-		entry = new double[m][n];
-		length = entry.length;
-	}
-	
-	//a second matrix constructor
-	public Matrix (double[] array)
-	{
-		entry = new double[array.length][1];
-		for (int i = 0; i < array.length; i++)
-			entry[i][0] = array[i];
-		length = entry.length;
-	}
-	
-	//fills a matrix to correctly encode the y0 stream	
-	public Matrix fillMatrixZero()
-	{
-		int length = this.length;
-		for (int i = 0; i < length; i++)
-			for (int j = 0; j < length; j++)
-				if (i >= j)
-				{
-					if (i == j)
-						this.entry[i][j] = 1;
-					if (i == j + 2)
-						this.entry[i][j] = 1;
-					if(i == j + 3)
-						this.entry[i][j] = 1;
-				}
-				else this.entry[i][j] = 0;
-		
-		return this;
-	}
-	
-	//fills a matrix to encode the y1 stream
-	public Matrix fillMatrixOne()
-	{
-		int length = this.length;
-		for (int i = 0; i < length; i++)
-			for (int j = 0; j < length; j++)
-				if (i >= j)
-				{
-					if (i == j)
-						this.entry[i][j] = 1;
-					if (i == j + 1)
-						this.entry[i][j] = 1;
-					if(i == j + 3)
-						this.entry[i][j] = 1;
-				}
-				else this.entry[i][j] = 0;
-		
-		return this;
-	}
+    //instance data for matrices
+    double[][] entry;
+    int length;
+    
+    //constructor for matrix object
+    public Matrix(int m, int n) {
+        entry = new double[m][n];
+        length = entry.length;
+    }
+    
+    //a second matrix constructor
+    public Matrix (double[] array) {
+        entry = new double[array.length][1];
+        for (int i = 0; i < array.length; i++)
+            entry[i][0] = array[i];
+        length = entry.length;
+    }
+    
+    //fills a matrix to correctly encode the y0 stream    
+    public Matrix fillMatrixZero() {
+        int length = this.length;
+        for (int i = 0; i < length; i++)
+            for (int j = 0; j < length; j++)
+                if (i >= j) {
+                    if (i == j) {
+                        this.entry[i][j] = 1;
+                    }
+                    if (i == j + 2) {
+                        this.entry[i][j] = 1; }
+                    if (i == j + 3) {
+                        this.entry[i][j] = 1; }
+                } else {
+                    this.entry[i][j] = 0;
+                }
+        return this;
+    }
+    
+    //fills a matrix to encode the y1 stream
+    public Matrix fillMatrixOne() {
+        int length = this.length;
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                if (i >= j) {
+                    if (i == j) {
+                        this.entry[i][j] = 1;
+                    }
+                    if (i == j + 1) {
+                        this.entry[i][j] = 1;
+                    }
+                    if(i == j + 3) {
+                        this.entry[i][j] = 1;
+                    }
+                } else {
+                    this.entry[i][j] = 0;
+                }
+            }
+        }
+        return this;
+    }
 
-	//randomly generates an input stream to encode
-	public static Matrix generateInput()
-	{
-		int i = 0;
-		Random generator = new Random();
-		Matrix x = new Matrix(Math.abs(generator.nextInt(10) + 4), 1);
-		for (i = 0; i < x.length; i++)
-			x.entry[i][0] = generator.nextInt(2);
-		x.entry[i - 1][0] = 0;
-		x.entry[i - 2][0] = 0;
-		x.entry[i - 3][0] = 0;
-		return x;
-	}
-	
-	//randomly generates an output stream to decode	
-	public static Matrix generateOutput()
-	{
-		int i = 0;
-		Random generator = new Random();
-		Matrix x = new Matrix(generator.nextInt(10) + 4, 1);
-		for (i = 0; i < x.length; i++)
-			x.entry[i][0] = generator.nextInt(2);
-		return x;
-	}
-	
-	//multiples two matrices together
-	public Matrix matrixMultiply(Matrix B)
-	{
-		int k,i,j;
-		Matrix C = new Matrix(this.length, B.entry[0].length);
-		for (k = 0; k < B.entry[0].length; k++)
-			for (i = 0; i < this.length; i++)
-				for (j = 0; j < B.length; j++)
-					C.entry[i][k] = ((this.entry[i][j] * B.entry[j][k]) + C.entry[i][k]) % 2;
-		
-		return C;
-	}
+    //randomly generates an input stream to encode
+    public static Matrix generateInput() {
+        int i = 0;
+        Random generator = new Random();
+        Matrix x = new Matrix(Math.abs(generator.nextInt(10) + 4), 1);
+        for (i = 0; i < x.length; i++) {
+            x.entry[i][0] = generator.nextInt(2);
+        }
+        x.entry[i - 1][0] = 0;
+        x.entry[i - 2][0] = 0;
+        x.entry[i - 3][0] = 0;
+        return x;
+    }
+    
+    //randomly generates an output stream to decode    
+    public static Matrix generateOutput() {
+        int i = 0;
+        Random generator = new Random();
+        Matrix x = new Matrix(generator.nextInt(10) + 4, 1);
+        for (i = 0; i < x.length; i++) {
+            x.entry[i][0] = generator.nextInt(2);
+        }
+        return x;
+    }
+    
+    //multiples two matrices together
+    public Matrix matrixMultiply(Matrix B) {
+        int k,i,j;
+        Matrix C = new Matrix(this.length, B.entry[0].length);
+        for (k = 0; k < B.entry[0].length; k++) {
+            for (i = 0; i < this.length; i++) {
+                for (j = 0; j < B.length; j++) {
+                    C.entry[i][k] = ((this.entry[i][j] * B.entry[j][k]) + C.entry[i][k]) % 2;
+                }
+            }
+        }
+        return C;
+    }
 
-	//Jacobi iteration for binary matrices
-	public Matrix jacobiIteration(Matrix A, int maxit)
-	{
-		int k, i, j;
-		
-		Matrix result = new Matrix (A.length, maxit);
-		for (k = 0; k < maxit; k++)
-			{
-			for (i = 0; i < this.length; i++)
-				{
-					double sum = 0;
-					for (j = 0; j < A.length; j++)
-					{
-						if (i != j)
-						{
-							if (k == 0)
-								sum = 0;
-							else
-								sum = ((A.entry[i][j] * result.entry[j][k - 1]) + sum) % 2;
-						}
-					}
-					result.entry[i][k] = (1 / A.entry[i][i]) * ((Math.abs(this.entry[i][0] - sum))) % 2;
-				}
-			} 
-		return result;
-	}
-	
-	//Gauss-Seidel Iteration for binary matrices
-	public Matrix gaussseidelIteration(Matrix A, int maxit)
-	{
-		int k, i, j;
-		
-		Matrix result = new Matrix (A.length, maxit);
-		for (k = 0; k < maxit; k++)
-			{
-			for (i = 0; i < this.length; i++)
-				{
-				double sum = 0;
-					for (j = 0; j < i-1; j++)
-					{
-						sum = ((A.entry[i][j] * result.entry[j][k]) + sum) % 2;	
-					}
-					for (j = i + 1; j < A.length; j++)
-					{	
-						if (k == 0)
-							sum = (A.entry[i][j] * 0 + sum) % 2;	
-						else	
-							sum = ((A.entry[i][j] * result.entry[j][k - 1]) + sum) % 2;
-					}
-					result.entry[i][k] = (1 / A.entry[i][i]) * ((Math.abs(this.entry[i][0] - sum))) % 2;
-				}
-			} 
-		return result;
-	}
-			
-	//Method to print out matrices to the console
-	public String toString()
-	{
-		for(int i = 0; i < this.length; i++)
-			{
-				for(int j = 0; j < entry[i].length; j++)	
-					System.out.print("\t" + entry[i][j]);
-				System.out.print("\n");
-			}
-		return (this.length + " by " + this.entry[0].length + "\n\n");
-	}
+    //Jacobi iteration for binary matrices
+    public Matrix jacobiIteration(Matrix A, int maxit) {
+        int k, i, j;
+        
+        Matrix result = new Matrix (A.length, maxit);
+        for (k = 0; k < maxit; k++) {
+            for (i = 0; i < this.length; i++) {
+                double sum = 0;
+                for (j = 0; j < A.length; j++) {
+                    if (i != j) {
+                        if (k == 0) {
+                            sum = 0;
+                        } else {
+                            sum = ((A.entry[i][j] * result.entry[j][k - 1]) + sum) % 2;
+                        }
+                    }
+                }
+                result.entry[i][k] = (1 / A.entry[i][i]) * ((Math.abs(this.entry[i][0] - sum))) % 2;
+            }
+        } 
+        return result;
+    }
+    
+    //Gauss-Seidel Iteration for binary matrices
+    public Matrix gaussseidelIteration(Matrix A, int maxit) {
+        int k, i, j;
+        
+        Matrix result = new Matrix (A.length, maxit);
+        for (k = 0; k < maxit; k++) {
+            for (i = 0; i < this.length; i++) {
+                double sum = 0;
+                for (j = 0; j < i-1; j++) {
+                    sum = ((A.entry[i][j] * result.entry[j][k]) + sum) % 2;    
+                }
+                for (j = i + 1; j < A.length; j++) {    
+                    if (k == 0) {
+                        sum = (A.entry[i][j] * 0 + sum) % 2;
+                    } else {
+                        sum = ((A.entry[i][j] * result.entry[j][k - 1]) + sum) % 2;
+                    }
+                    result.entry[i][k] = (1 / A.entry[i][i]) * ((Math.abs(this.entry[i][0] - sum))) % 2;
+                }
+            }
+        }
+        return result;
+    }
+            
+    //Method to print out matrices to the console
+    public String toString() {
+        for (int i = 0; i < this.length; i++) {
+            for(int j = 0; j < entry[i].length; j++) {
+                System.out.print("\t" + entry[i][j]);
+            }
+            System.out.print("\n");
+        }
+        return (this.length + " by " + this.entry[0].length + "\n\n");
+    }
 }
